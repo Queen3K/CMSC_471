a1a1850280ca941253b1b9276acb85b64248cd5e
Mitchell Brown <Queen3K@proton.me>
2026-04-16 17:32:40 -0400

    Made HTML for Hello World AB#5

---
+++ .aws-sam/build/StaticProxyFunction/proxy.py
@@ -0,0 +1,21 @@
+import boto3
+import os
+import json
+
+s3 = boto3.client('s3')
+
+def proxy_handler(event, context):
+    print(json.dumps(event))
+
+    bucket = os.environ['BUCKET_BUCKET_NAME']
+
+    response = s3.get_object(Bucket=bucket, Key='index.html')
+    html_body = response['Body'].read().decode('utf-8')
+
+    return {
+        'statusCode': 200,
+        'body': html_body,
+        'headers': {
+            'Content-Type': 'text/html'
+        }
+    }
